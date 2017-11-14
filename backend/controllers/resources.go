0244fd5770c406c0b356c2fe8d2dca01e0550207
Andreas Winter <andi@localhost.localdomain>
2017-11-14 16:32:22 +0100

    Implement auth function

---
+++ backend/controllers/resources.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"github.com/wandi34/wallets-as-a-service/backend/models"
+)
+
+//Models for JSON resources
+type (
+	//For Post - /user/register
+	UserResource struct {
+		Data models.User `json:"data"`
+	}
+	//For Post - /user/login
+	LoginResource struct {
+		Data LoginModel `json:"data"`
+	}
+	//Response for authorized user Post - /user/login
+	AuthUserResource struct {
+		Data AuthUserModel `json:"data"`
+	}
+	//Model for authentication
+	LoginModel struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	//Model for authorized user with access token
+	AuthUserModel struct {
+		User  models.User `json:"user"`
+		Token string      `json:"token"`
+	}
+)
