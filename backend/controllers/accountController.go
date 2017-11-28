06451588e313750a4e4a90e7f2a2585c3ff9e223
Andreas Winter <awi@adorsys.de>
2017-11-28 10:32:07 +0100

    Add GET Endpoint for accounts

--- backend/controllers/accountController.go
+++ backend/controllers/accountController.go
@@ -8,20 +8,47 @@ import (
 	"fmt"
 	"encoding/json"
 	"github.com/wandi34/wallets-as-a-service/backend/common"
+	"github.com/wandi34/wallets-as-a-service/backend/models"
+	"gopkg.in/mgo.v2/bson"
+	"github.com/gorilla/mux"
 )
 
 
 var bcy = gobcy.API{"2aa27c3912c047f2baa7e932cfc453e7", "bcy", "test"}
 
 func GetAccounts(w http.ResponseWriter, r *http.Request) {
+	// Get userId from URL
+	vars := mux.Vars(r)
+	userId := bson.ObjectIdHex(vars["id"])
+
+	// Get Account for userId
+	context := NewContext()
+	defer context.Close()
+	col := context.DbCollection("accounts")
+	repo := &data.AccountRepository{C: col}
+	// Authenticate the login user
+	result := models.Account{}
+	err := repo.C.Find(bson.M{"userid": userId}).One(&result)
+	if err != nil {
+		common.DisplayAppError(
+			w,
+			err,
+			"An unexpected error has occurred",
+			500,
+		)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	j, err := json.Marshal(AccountResource{Data: result})
+	w.Write(j)
 
 }
 
 func CreateAccount(w http.ResponseWriter, r *http.Request) {
 	var dataResource CreateWalletResource
-	// Decode the incoming User json
+	// Decode the incoming CreateAccount json
 	err := json.NewDecoder(r.Body).Decode(&dataResource)
 	if err != nil {
 		common.DisplayAppError(
